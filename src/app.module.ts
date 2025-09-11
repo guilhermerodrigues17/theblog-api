0e9170c772f65f60ef1ed2691c4c6ebb31214921
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-11 18:55:48 -0300

    chore: add TypeORM database connection configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,28 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.DB_SYNCHRONIZE === '1',
+      autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
