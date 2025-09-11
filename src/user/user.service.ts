9e0549779495b7d0779d2b2eb5383a581f149f5e
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-11 19:57:29 -0300

    feat(user): implements create method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from 'src/common/hashing/hashing-service.interface';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const exists = await this.userRepository.exists({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (exists) {
+      throw new ConflictException('Email already exists in database');
+    }
+
+    const hashedPassword = await this.hashingService.hash(
+      createUserDto.password,
+    );
+
+    const newUser: CreateUserDto = {
+      name: createUserDto.name,
+      email: createUserDto.email,
+      password: hashedPassword,
+    };
+
+    const userCreated = await this.userRepository.save(newUser);
+    return userCreated;
   }
 
   findAll() {
