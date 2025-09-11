9f2067da1297f970583de6e1b49b3aed3f405ba3
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-11 19:56:12 -0300

    feat(user): implements user entity and CreateUserDto class

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({ message: 'field name must be a string' })
+  @IsNotEmpty({ message: 'field name cannot be empty' })
+  @MinLength(3, { message: 'name is too short' })
+  @MaxLength(100, { message: 'name is too long' })
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty({ message: 'field email cannot be empty' })
+  email: string;
+
+  @IsString({ message: 'field password must be a string' })
+  @IsNotEmpty({ message: 'field password cannot be empty' })
+  password: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'tb_user' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  forceLogout: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
