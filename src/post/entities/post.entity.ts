6bc2c9e1dd22169b9c25c74f8ba7480dbf9218d1
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-16 10:48:46 -0300

    feat(post): implements post entity

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,37 @@
-export class Post {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'tb_post' })
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @Column()
+  excerpt: string;
+
+  @Column('text')
+  content: string;
+
+  @Column({ nullable: true, name: 'cover_image_url' })
+  coverImageUrl: string;
+
+  @Column({ default: false })
+  published: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
