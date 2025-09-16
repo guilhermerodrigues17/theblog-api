113430eb6c28be9f8dd91e256a921f0adc82b9dd
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-16 18:16:24 -0300

    refactor(utils): moving generateRandomSuffix to a separate file

--- src/common/utils/create-slug-from-text.ts
+++ src/common/utils/create-slug-from-text.ts
@@ -1,10 +1,7 @@
+import { generateRandomSuffix } from './generate-random-suffix';
 import { slugify } from './slugify';
 
 export function createSlugFromText(text: string) {
   const slug = slugify(text);
   return `${slug}-${generateRandomSuffix()}`;
-}
-
-function generateRandomSuffix() {
-  return Math.random().toString(36).substring(2, 8);
 }
